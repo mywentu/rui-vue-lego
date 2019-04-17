d366157b21ed298b14ac8fe220b7a9eee19f844a
文彬 <wenbin@rong360-MacBook56.local>
2019-04-17 21:52:29 +0800

    base64添加

--- build/build-style.js
+++ build/build-style.js
@@ -1,10 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const cssBase64 = require('gulp-base64');
 
 
 gulp.task('sass', function () {
   return gulp.src('../packages/theme//index.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(cssBase64({
+      maxImageSize: 8*1024
+    }))
     .pipe(gulp.dest('../dist/styles'));
 });
 
