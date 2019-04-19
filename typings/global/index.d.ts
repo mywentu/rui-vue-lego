4a49e2fcea435924c598cb579792ea91de8baaae
文彬 <wenbin@rong360-MacBook56.local>
2019-04-19 12:03:19 +0800

    package修改

--- typings/global/index.d.ts
+++ typings/global/index.d.ts
@@ -7,10 +7,3 @@ declare module "vue/types/vue" {
     $route: Route;
   }
 }
-declare module "iview/dist/types/modal" {
-  interface Modal {
-    /** 错误弹窗  */
-    error(configs: { title?: string, content?: string}) : void
-  }
-  export const Modal: Modal
-}
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,13 +1,4 @@
-/**
- * For module import or split code use require
- */
-
 declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
-}
-
-declare interface Date {
-  /** 传入形如yyyy-MM-dd格式字符串，输出格式化后的时间 */
-  format(string): string;
 }
