32c642b45483b92a61b631043565667ccfc011da
文彬 <wenbin@rong360-MacBook56.local>
2019-04-26 18:01:29 +0800

    删除废弃文件

--- example/router/demo.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  dialog: () => import('../demos/dialog.vue'),
-  loading: () => import('../demos/loading.vue'),
-  picker: () => import('../demos/picker.vue'),
-  toast: () => import('../demos/toast.vue'),
-}
--- example/router/docs.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  dialog: () => import('../../packages/dialog/readme.md'),
-  loading: () => import('../../packages/loading/readme.md'),
-  picker: () => import('../../packages/picker/readme.md'),
-  toast: () => import('../../packages/toast/readme.md'),
-}
